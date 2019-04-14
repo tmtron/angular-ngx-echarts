6eb751aa343accc9044fe76302b0b32d6f70ccbd
tmtron <martin.trummer@tmtron.com>
2019-04-14 14:58:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngx-echarts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngx-echarts)
---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, DoCheck {
+  name = 'Angular';
+
+  options: any;
+
+  ngDoCheck(): void {
+    console.log('doCheck');
+  }
+
+  getTooltipFormatter() {
+    return (params) => {
+          return '<div style="width:300px; height: 400px">working</div>'
+        };
+  }
+
+  ngOnInit() {
+    const xAxisData = [];
+    const data1 = [];
+    const data2 = [];
+
+    for (let i = 0; i < 100; i++) {
+      xAxisData.push('category' + i);
+      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    }
+
+    this.options = {
+      tooltip: {
+        // formatter: params => {
+        //   return '<div style="width:300px; height: 400px">working</div>';
+        // },
+        // formatter: this.getTooltipFormatter(),
+        confine: true
+      },
+      xAxis: {
+        data: xAxisData
+      },
+      yAxis: {},
+      series: [
+        {
+          type: 'bar',
+          data: data1
+        },
+        {
+          type: 'bar',
+          data: data2
+        }
+      ]
+    };
+  }
+}
